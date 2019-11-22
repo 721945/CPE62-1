1d79e229e0c703582fdd4bf56452276703c77bdd
guide725 <44542388+guide725@users.noreply.github.com>
2019-11-23 00:29:57 +0700

    Add files via upload

---
+++ problem2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    double A;
+
+    scanf("%lf",&A);
+
+    printf("Use \%%d :%d\n",A);
+    printf("Use \%%f :%f\n",A);
+    printf("Use \%%lf :%lf\n",A);
+
+    return 0;
+}
